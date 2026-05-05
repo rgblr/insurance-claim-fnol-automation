a57e63bc74df3dca141b51a568c21a92f42cc8d5
rgblr <rakeshgu74@gmail.com>
2026-05-05 12:59:50 +0530

    Update vite.config.ts
    force lovable rebuild from github

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// force lovable rebuild from git-hub
 // @lovable.dev/vite-tanstack-config already includes the following — do NOT add them manually
 // or the app will break with duplicate plugins:
 //   - tanstackStart, viteReact, tailwindcss, tsConfigPaths, cloudflare (build-only),
